138eab1151fb43876c141202c340d893b098b9d5
Lotte de Wit <lizzelotte9@gmail.com>
2026-06-04 20:12:56 +0200

    Les 5 afgerond

---
+++ Les5.py/huiswerk_les5.py
@@ -0,0 +1,10 @@
+aantal = 10
+prijs = 0.50
+totaal = aantal * prijs
+print(totaal)
+for i in range (10, 20, 1):
+    print(f"{i} maal 2 is {i*2}")
+print("lift off!")
+leeftijd = 30
+naam = "Deborah"
+print(f"Haar naam is {naam}, ze is {leeftijd} jaar oud.")
