b332807f21815cd2e70377e6da725a7200b6fee9
Lotte de Wit <lizzelotte9@gmail.com>
2026-06-13 10:38:12 +0200

    nieuwe wijzingen les6

--- Les6/test.py
+++ Les6/test.py
@@ -1,3 +1,7 @@
 mijn_lijst = ["Wafels", "Softijs", "Schepijs", "Pannenkoeken"]
+mijn_lijst.append("Muffins")
+for item in mijn_lijst:
+    print(f"Wij verkopen {item}")
+mijn_lijst.append("Cola")
 for item in mijn_lijst:
     print(f"Wij verkopen {item}")
